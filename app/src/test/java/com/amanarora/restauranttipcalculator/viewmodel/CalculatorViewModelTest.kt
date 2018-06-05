8e496d968800b7eed033f4cb372bd2687209a991
amanarora <aman@tokostudios.com>
2018-06-06 02:45:53 +0530

    added test for CalculatorViewModer

--- app/src/test/java/com/amanarora/restauranttipcalculator/viewmodel/CalculatorViewModelTest.kt
+++ app/src/test/java/com/amanarora/restauranttipcalculator/viewmodel/CalculatorViewModelTest.kt
@@ -1,16 +1,26 @@
 package com.amanarora.restauranttipcalculator.viewmodel
 
+import com.amanarora.restauranttipcalculator.model.RestaurantCalculator
+import com.amanarora.restauranttipcalculator.model.TipCalculation
 import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
 
 class CalculatorViewModelTest {
 
     lateinit var calculatorViewModel : CalculatorViewModel
 
+    @Mock
+    lateinit var mockCalculator : RestaurantCalculator
+
     @Before
     fun setup() {
-        calculatorViewModel = CalculatorViewModel()
+        MockitoAnnotations.initMocks(this)
+        calculatorViewModel = CalculatorViewModel(mockCalculator)
     }
 
     @Test
@@ -18,12 +28,38 @@ class CalculatorViewModelTest {
         calculatorViewModel.inputCheckAmount = "10.00"
         calculatorViewModel.inputTipPercentage = "15"
 
+        val stub = TipCalculation(checkAmount = 10.00, tipAmount = 1.5, grandTotal = 11.5)
+        `when`(mockCalculator.calculateTip(10.00,15)).thenReturn(stub)
+
         calculatorViewModel.calculateTip()
 
-        assertEquals(10.00, calculatorViewModel.tipCalculation.checkAmount)
+        assertEquals(stub, calculatorViewModel.tipCalculation)
+       /* assertEquals(10.00, calculatorViewModel.tipCalculation.checkAmount)
         assertEquals(15, calculatorViewModel.tipCalculation.tipPct)
-        assertEquals(11.50, calculatorViewModel.tipCalculation.grandTotal)
+        assertEquals(11.50, calculatorViewModel.tipCalculation.grandTotal)*/
     }
+
+    @Test
+    fun testCalculateTipBadTipPercent() {
+        calculatorViewModel.inputCheckAmount = "10.00"
+        calculatorViewModel.inputTipPercentage = ""
+
+        calculatorViewModel.calculateTip()
+
+        verify(mockCalculator, never()).calculateTip(ArgumentMatchers.anyDouble(), ArgumentMatchers.anyInt())
+    }
+
+    @Test
+    fun testCalculateTipBadCheckAmount() {
+        calculatorViewModel.inputCheckAmount = ""
+        calculatorViewModel.inputTipPercentage = "15"
+
+        calculatorViewModel.calculateTip()
+
+        verify(mockCalculator, never()).calculateTip(ArgumentMatchers.anyDouble(), ArgumentMatchers.anyInt())
+    }
+
+
 
 
 }
